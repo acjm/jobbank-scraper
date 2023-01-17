da53eeca817d55ec733fb511d3d3a9303f71ba98
Chafai Ayman <chafaiayman@gmail.com>
2023-01-17 20:38:40 +0000

    Re-write the index.js file
    Dude, I wrote another script, it accesses every job post and fetches the titles (you can use selectors to get the rest of the data ) for the first 20 records ( you can't load everything, there are too many posts, and you need another script to keep loading that without having a runtime error, CRON JOB )

--- index.js
+++ index.js
@@ -1,40 +1,74 @@
+
+
 const puppeteer = require('puppeteer');
-const fs = require('fs/promises');
 
-async function start(){
-    // Launch a new browser instance and create a new page
-    const browser = await puppeteer.launch({
-        headless: false
+const url = 'https://www.jobbank.gc.ca/jobsearch/jobsearch?fcid=24789&fcid=25599&fcid=25725&fn=1242&fn=1243&term=administrative+assistant&page=1&sort=D';
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  // Get all job links
+  let links = await page.evaluate(() => {
+    const listings = Array.from(document.querySelectorAll('.resultJobItem'));
+    return listings.map(listing => listing.href);
+  });
+  // check if there's a load more button
+  let loadMoreButton = await page.$('#moreresultbutton');
+  let jobCounter = links.length; 
+  /**
+   * If there's a load more button, click it and wait for the new jobs to load
+   * 20 because loads infinite jobs, this take a long time to load
+   * think of a cron job to get the jobs every 24 hours
+   * 
+   */
+   while (loadMoreButton && jobCounter < 20) {
+    // wait for the button to be clickable
+    await page.waitForSelector('#moreresultbutton', {visible: true});
+    // check if the button is disabled
+    const isDisabled = await page.evaluate(() => {
+      return document.querySelector("#moreresultbutton").disabled;
     });
-    const page = await browser.newPage();
-    // Navigate to the www.jobbank.gc.ca website
-    await page.goto("https://www.jobbank.gc.ca/jobsearch/jobsearch?fcid=24789&fcid=25599&fcid=25725&fn=1242&fn=1243&term=administrative+assistant&page=1&sort=D");
-    await page.click(".command-result-filter-overlay");
-    // Save all the job posts urls on the page to an array
-    const jobUrls = await page.evaluate(() => Array.from(document.querySelectorAll('.results-jobs article > a'), article => article.href));
     
+    if (!isDisabled) {
+      await loadMoreButton.click();
+      // wait for new jobs to load
+      await page.waitForSelector('.job-listing', { timeout: 5000 });
+      // get new links after loading more jobs
+      let newLinks = await page.evaluate(() => {
+        const listings = Array.from(document.querySelectorAll('.resultJobItem'));
+        return listings.map(listing => listing.href);
+      });
+      links = links.concat(newLinks);
+      jobCounter = links.length;
+    }
+    loadMoreButton = await page.$('#moreresultbutton');
+  }
 
-    for (let i = 0; i < jobUrls.length; i++) {
-        // Visit each job post page
-        await page.goto(jobUrls[i]);
-        // Show employer's email address
-        await page.click("#applynowbutton");
-        // Wait for employer's email address to show
-        await page.waitForSelector("#howtoapply > p > a");
-        
-        // Job post data
-        const jobTitle =  await page.$eval('span[property=title]', el => el.innerText);
-        const location =  await page.$eval('span[property=address]', el => el.innerText);
-        const salary =  await page.$eval('span[property=baseSalary]', el => el.parentElement.innerText);
-        const responsibilities =  await page.$eval('div[property=responsibilities]', el => el.innerText);
-        const experienceRequirements =  await page.$eval('#comparisonchart > p:nth-child(6)', el => el.innerText);
-        const emailAddress =  await page.$eval('#howtoapply > p > a', el => el.innerText);
-        
-        // Output data
-        console.log(jobTitle, location, salary, responsibilities, experienceRequirements, emailAddress);
-        await fs.writeFile("jbdata.json", JSON.stringify({jobTitle, location, salary, responsibilities, experienceRequirements, emailAddress}), 'utf8');
-      }
+  links = links.slice(0, 20);
 
-}
+  console.log(links);
+  // For each link, go to the job details page, scrape the data and log it
+  for (let link of links) {
+    await page.goto(link);
+    const jobDetails = await page.evaluate(() => {
+        /**
+         * Scrape the data from the page
+         * @return {Object} job details
+         * 
+         * You can use any selector you want to get the data from the page
+         */
+        return {
+         title: document.querySelector('h1#wb-cont span:first-child').innerText,
+        };
+    });
 
-start();
+    console.log(jobDetails);
+    /***
+     * Save the job details to a database/json or csv file || whatever you want :D 
+     */
+  }
+
+  await browser.close();
+})();
